c6d770ee736a4af46741e6b31556038aab862c35
TBLinhDan <tblinhdan@gmail.com>
2024-06-11 08:48:02 +0700

    delete file from local repo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # calculate sum a, b
-def sum(a, b):
-    return a + b
+def main():
+    def sum(a, b):
+        return a + b
 
 if __name__ == "__main__":
-    print(f"Total: {sum(3, 4)}")
+    sum = sum(3,4)
+    print(f"Total: {sum}")
